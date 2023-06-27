0c99e71de9b728504862bad640607a6c3a419428
OntiretseMotene <ontiretse@codespace.co.za>
2023-06-27 13:34:44 +0200

    DWA 4 Submission

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -3,39 +3,43 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  
+
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  
 
+  const trimDivident = dividend.trim() //
+  const trimDivider = divider.trim() //
 
+  if (trimDivident == "" || trimDivider == "") // User Story 4: Validation when values are missing. 
+  {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    displayError("Division not performed. Both values are required in inputs. Try again");
+  }
+  else if (trimDivident < 0 || trimDivider < 0) // User story 5: An invalid division should log an error in the console
+  {
 
-if (dividend == "" || divider == " ") // User Story 3: Validation when values are missing. 
-{
-  result.innerText = "Division not performed. Both values are required in inputs. Try again";
-} 
-else if (dividend < 0 || divider < 0) // User story 4: An invalid division should log an error in the console
-{
-  
-  result.innerHTML = "Division not performed. Invalid number provided. Try again";
-  console.log();
-} 
-else if ( isNaN(dividend) || isNaN(divider) ) // User Story 5: Providing anything that is not a number should crash the program
-{
-  console.error (
-    new Error("Something critical went wrong. Please reload the page." ));
+    result.innerHTML = "Division not performed. Invalid number provided. Try again";
+    displayError("Division not performed. Invalid number provided. Try again");
+  }
+  else if (isNaN(trimDivident) || isNaN(trimDivider)) // User Story 6: Providing anything that is not a number should crash the program
+  {
+    
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page.</h1>";
+    displayError("Something critical went wrong. Please reload the page")
+  }
+  else {
+    result.innerText = Math.floor(trimDivident / trimDivider) // User Story 3: Dividing numbers result in a decimal number. 
 
-  document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page.</h1>";
-} 
-else 
-{
-  result.innerText = Math.floor(dividend / divider)
-  
-}
+  }
 
-
+    
+    function displayError(message) {
+      return console.error(new Error(message))
+    }
 });
+
+
 
 
 
